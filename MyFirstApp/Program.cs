f974cd34667b5ad393fe8f0497396cb035cccb5a
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-09-02 13:37:57 +0300

    Add project files.

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,27 @@
+﻿// це рядковий коментар
+// Ctrl + F5 - запуск програми
+
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello! How are you?");
+
+// cw + TAB = Console.WriteLine();
+Console.WriteLine("Enter your age:");
+
+// WriteLine - ставить новий рядок вкінці
+// Write - не переводить на новий рядок
+Console.WriteLine("My name is Vlad!");
+Console.WriteLine("My name is Vlad!");
+
+Console.Write("Good");
+Console.Write("Good");
+
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+// Escape sequences: \n \t \' \"
+Console.WriteLine("\n\nПривіт!\nЯк\tв тебе\t\t\"справи\"?");
