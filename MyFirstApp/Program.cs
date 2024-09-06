f9611eb19f4f7db73c089ac228c3e85bed2a0d6a
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-09-06 14:01:48 +0300

    Змінні та типи даних

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,4 +1,5 @@
-﻿// це рядковий коментар
+﻿// ------------------------- вивід на консоль
+// це рядковий коментар
 // Ctrl + F5 - запуск програми
 
 using System.Text;
@@ -25,3 +26,31 @@ Console.ForegroundColor = ConsoleColor.Yellow;
 
 // Escape sequences: \n \t \' \"
 Console.WriteLine("\n\nПривіт!\nЯк\tв тебе\t\t\"справи\"?");
+
+// скидуємо кольори на стандартні
+Console.ResetColor();
+
+// ------------------------- змінні
+// створення змінної: тип назва = значення;
+
+// -------- типи даних:
+// string - пошта, адреса, імя, колір
+// short/int float/double - вік, ціна, ріст, ширина
+
+// формат назви: accountLogin productPrice 
+string username = "anonymous";
+
+username = "Blalba";
+username = "no name";
+
+Console.Write("Enter your name: ");
+username = Console.ReadLine();
+
+// інтерполяція рядка: $"...{вираз}..."
+Console.WriteLine($"Hello, dear {username}!");
+
+// ------------------------- ЗАВДАННЯ: запитати рік народження та показати вік
+Console.Write("Enter your birth year: ");
+int dateOfBirth = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"You are {2024 - dateOfBirth} years old!");
