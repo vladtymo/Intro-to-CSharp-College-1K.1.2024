e2db201e4e2c1f8f1f10c1b0bab3899242f23090
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-09-09 13:46:33 +0300

    Calculate task examples.

---
+++ 02_In_Out/Program.cs
@@ -0,0 +1,29 @@
+﻿//Console.WriteLine(2 + 2 * 2);   // 6
+//Console.WriteLine((2 + 2) * 2); // 8
+
+//------------- Завдання: користувач вводить милі, а програма повинна відобразити в км
+
+// 1. введення значення
+// 2. обрахунок значення
+// 3. показ результату
+
+//Console.WriteLine("------------- Miles to Km Converter -------------");
+
+//Console.Write("Enter miles: ");
+
+//double miles = double.Parse(Console.ReadLine()!);
+
+//double kms = miles * 1.60934;
+
+//Console.WriteLine($"Result: {kms} km");
+
+//------------- Завдання: користувач вводить радіус кола, визначити та показати його площу
+
+Console.WriteLine("------------- Calculate Circle Area -------------");
+
+Console.Write("Enter radius (cm): ");
+
+double r = double.Parse(Console.ReadLine()!);
+double area = Math.PI * Math.Pow(r, 2);
+
+Console.WriteLine($"Circle area: {area} cm");
