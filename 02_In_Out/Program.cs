40dbb6b17b1153247767dca1a66b1c211a658836
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-09-13 14:07:02 +0300

    Done new task

--- 02_In_Out/Program.cs
+++ 02_In_Out/Program.cs
@@ -7,15 +7,15 @@
 // 2. обрахунок значення
 // 3. показ результату
 
-//Console.WriteLine("------------- Miles to Km Converter -------------");
+Console.WriteLine("------------- Miles to Km Converter -------------");
 
-//Console.Write("Enter miles: ");
+Console.Write("Enter miles: ");
 
-//double miles = double.Parse(Console.ReadLine()!);
+double miles = double.Parse(Console.ReadLine()!);
 
-//double kms = miles * 1.60934;
+double kms = miles * 1.60934;
 
-//Console.WriteLine($"Result: {kms} km");
+Console.WriteLine($"Result: {kms} km");
 
 //------------- Завдання: користувач вводить радіус кола, визначити та показати його площу
 
@@ -29,3 +29,14 @@ double area = Math.PI * Math.Pow(r, 2);
 double rounded = Math.Round(area, 1);
 
 Console.WriteLine($"Circle area: {rounded} cm");
+
+//------------- Завдання: користувач вводить периметр квадрату, а додаток відображає довжину сторони
+
+Console.WriteLine("------------- Calculate Side Length -------------");
+
+Console.Write("Enter P (cm): ");
+
+double p = double.Parse(Console.ReadLine()!);
+double side = p / 4;
+
+Console.WriteLine($"Square side length: {side} cm");
