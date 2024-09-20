eb263385f75654c7e9cc14fa0be14a480e15b223
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-09-20 13:39:18 +0300

    Логічні оператори

---
+++ 03_logic/Program.cs
@@ -0,0 +1,22 @@
+﻿// Завдання: користувач вводить рік народження, перевірити чи може він купити вино
+
+Console.Write("Enter your birth year: ");
+
+int birthYear = int.Parse(Console.ReadLine());
+int age = 2024 - birthYear;
+
+// логічні оператори: > < >= <= == !=
+
+// перевірити чи користувач повнолітній
+if(age >= 18)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("You can buy a wine!");
+    Console.ResetColor();
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("You can not buy a wine! Suggest milk)");
+    Console.ResetColor();
+}
