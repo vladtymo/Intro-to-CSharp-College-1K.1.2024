876661ca24dc0b662400508af1d54f52fe70819b
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-10-04 13:40:52 +0300

    Intro to for loop

---
+++ 04_loops/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Hello");
+Console.WriteLine("Hello");
+Console.WriteLine("Hello");
+Console.WriteLine("Hello");
+Console.WriteLine("Hello");
+
+Console.Write("Enter iteration count: ");
+int number = int.Parse(Console.ReadLine()!);
+
+// for - цикл який дозволяє створити лічильник
+// шаблон:
+/* 
+    for(створення змінний [1]; умови [2]; вираз [4]) 
+    { 
+        код [3]
+    }
+*/
+
+for (int count = 0; count < number; count++)
+{
+    Console.WriteLine("...Iteration...");
+}
