83361ce33b1f0ded2240fbfb1438f559f0064b56
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-10-14 14:24:09 +0300

    Create simple 15 puzzle game

---
+++ 06_arrays/Program.cs
@@ -0,0 +1,43 @@
+﻿// Робота з масивами
+
+int price1 = 1200;
+int price2 = 1200;
+int price3 = 1200;
+int price4 = 1200;
+int price5 = 1200;
+
+int[] prices = [1200, 340, 99, 145000];
+
+Console.WriteLine("Products: " + prices.Length);
+Console.WriteLine("Total price: " + prices.Sum());
+
+// масив кольорів
+string[] colors = ["red", "blue", "green", "yellow", "orange", "purple" ];
+
+// index - 0...
+for (int i = 0; i < prices.Length; ++i)
+{
+    Console.WriteLine("Color: " + colors[i]);
+}
+
+// two-dimension array
+const int rows = 4;
+const int columns = 5;
+int[,] map = new int[rows, columns]
+{
+    { 1, 2, 4, 2, 5 },
+    { 3, 4, 4, 2, 5 },
+    { 5, 6, 4, 2, 5 },
+    { 7, 8, 4, 2, 5 }
+};
+
+Console.WriteLine(map[3, 1]); // 8
+
+for (int r = 0; r < rows; r++)
+{
+    for (int c = 0; c < columns; c++)
+    {
+        Console.Write(map[r, c] + " ");
+    }
+    Console.WriteLine();
+}
---
+++ 07_puzzle/Program.cs
@@ -0,0 +1,58 @@
+﻿// створення гри П'ятнашки
+
+const int size = 4;
+int emptyX = 2;
+int emptyY = 2;
+
+int[,] puzzle = new int[size, size]
+{
+    {1, 2, 3, 4},
+    {5, 6, 7, 8},
+    {9, 10, 0, 12},
+    {13, 14, 15, 11}
+};
+ShowPuzzle();
+
+while (true)
+{
+    var key = Console.ReadKey().Key;
+    
+    switch (key)
+    {
+        case ConsoleKey.LeftArrow:
+            // left
+            Swap(ref puzzle[emptyY, emptyX], ref puzzle[emptyY, emptyX + 1]);
+            ++emptyX;
+            break;
+        case ConsoleKey.RightArrow:
+            // right
+            Swap(ref puzzle[emptyY, emptyX], ref puzzle[emptyY, emptyX - 1]);
+            --emptyX;
+            break;
+    }
+
+    Console.Clear();
+    ShowPuzzle();
+}
+
+void ShowPuzzle()
+{
+    for (int r = 0; r < size; r++)
+    {
+        for (int c = 0; c < size; c++)
+        {
+            if (puzzle[r, c] == 0)
+                Console.Write("[  ] ");
+            else
+                Console.Write($"[{puzzle[r, c],2}] ");
+        }
+        Console.WriteLine();
+    }
+}
+
+static void Swap(ref int first, ref int second)
+{
+    int temp = first;
+    first = second;
+    second = temp;
+}
