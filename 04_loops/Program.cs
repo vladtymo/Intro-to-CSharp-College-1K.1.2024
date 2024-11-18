003af58a587ef992384d7d7ac8184f8f403f4a80
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-11-18 13:40:25 +0200

    Methods and parameters

--- 04_loops/Program.cs
+++ 04_loops/Program.cs
@@ -1,45 +1,71 @@
-﻿Console.WriteLine("Hello");
-Console.WriteLine("Hello");
-Console.WriteLine("Hello");
-Console.WriteLine("Hello");
-Console.WriteLine("Hello");
+﻿// Console.WriteLine("Hello");
+// Console.WriteLine("Hello");
+// Console.WriteLine("Hello");
+// Console.WriteLine("Hello");
+// Console.WriteLine("Hello");
+//
+// Console.Write("Enter iteration count: ");
+// int number = int.Parse(Console.ReadLine()!); // 10
+//
+// // for - цикл який дозволяє створити лічильник
+// // шаблон:
+// /* 
+//     for(створення змінний [1]; умови [2]; вираз [4]) 
+//     { 
+//         код [3]
+//     }
+// */
+//
+// for (int count = 0; count < 10; count++)
+// {
+//     Console.WriteLine("...Iteration...");
+// }
+//
+// // вічний цикл
+//
+// Console.Write("Enter your name: ");
+// string name = Console.ReadLine();
+//
+// while (!char.IsUpper(name[0]))
+// {
+//     Console.WriteLine("Name must start with upper case letter!");
+//     Console.Write("Enter name again: ");
+//     name = Console.ReadLine();
+// }
+//
+//
+// int level = 0;
+//
+// do 
+// {
+//     Console.Write("Enter game level (1-3): ");
+//     level = int.Parse(Console.ReadLine());
+// } while (level < 1 || level > 3);
+//
+// Console.WriteLine("Have a good day!");
 
-Console.Write("Enter iteration count: ");
-int number = int.Parse(Console.ReadLine()!); // 10
+// -------- цикли
+// while(умову) { }
 
-// for - цикл який дозволяє створити лічильник
-// шаблон:
-/* 
-    for(створення змінний [1]; умови [2]; вираз [4]) 
-    { 
-        код [3]
-    }
-*/
+Console.Write("Enter age: ");
+int age = int.Parse(Console.ReadLine());
 
-for (int count = 0; count < 10; count++)
+while (age < 0)
 {
-    Console.WriteLine("...Iteration...");
+    Console.WriteLine("Invalid age. Please try again: ");
+    age = int.Parse(Console.ReadLine());
 }
 
-// вічний цикл
-
-Console.Write("Enter your name: ");
-string name = Console.ReadLine();
-
-while (!char.IsUpper(name[0]))
-{
-    Console.WriteLine("Name must start with upper case letter!");
-    Console.Write("Enter name again: ");
-    name = Console.ReadLine();
-}
-
-
-int level = 0;
-
+int discount;
 do 
 {
-    Console.Write("Enter game level (1-3): ");
-    level = int.Parse(Console.ReadLine());
-} while (level < 1 || level > 3);
+    Console.WriteLine("Enter discount (0-100%): ");
+    discount = int.Parse(Console.ReadLine());
+} while(discount < 0 || discount > 100);
 
-Console.WriteLine("Have a good day!");
+int width = 12;
+
+for (int i = 0; i < width; i++)
+{
+    Console.Write("|");
+}
---
+++ 10_methods/Program.cs
@@ -0,0 +1,33 @@
+﻿// method: return_type name(parameters) { code }
+// void - нічого
+
+void PrintMessage(int count, string separator)
+{
+    for (int i = 0; i < count; ++i)
+    {
+        Console.Write("Hello" + separator);
+    }
+    Console.WriteLine();
+}
+
+PrintMessage(7, "-");
+PrintMessage(2, " ");
+
+Console.Write("Enter count: ");
+int c = int.Parse(Console.ReadLine());
+PrintMessage(c, ", ");
+
+// ...code
+
+void PrintRectangle(int w, int h, char filler, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    for (int i = 0; i < h; i++)
+    {
+        Console.WriteLine(new string(filler, w));
+    }
+    Console.ResetColor();
+}
+
+PrintRectangle(10, 7, '%', ConsoleColor.Yellow);
+PrintRectangle(3, 3, '#', ConsoleColor.Blue);
