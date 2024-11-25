d339a3bf53e2ccf56c278632830a6b44932f5295
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-11-25 14:09:30 +0200

    Intro to OOP and classes

---
+++ 11_oop/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Snake snake = new();
+        
+        snake.Print();
+
+        Console.WriteLine();
+    }
+    
+    
+}
+
+class Snake
+{
+    // properties (numbers, strings and etc)
+    public int size = 1;
+    public ConsoleColor color = ConsoleColor.Red;
+    public int x = 0;
+    public int y = 0;
+    public char symbol = '@';
+
+    // methods (actions)
+    public void Print()
+    {
+        Console.ForegroundColor = color;
+        Console.SetCursorPosition(x, y);
+
+        Console.Write(symbol);
+        
+        Console.ResetColor();
+    }
+}
