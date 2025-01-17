11d11086d4c2fdcf3e15f77f45089c6fbeef5105
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2025-01-17 13:31:20 +0200

    Stack and Queue

---
+++ 18_stack_queue/Program.cs
@@ -0,0 +1,34 @@
+﻿// ----------- Stack -----------
+Stack<string> stack = new();
+
+stack.Push("Ivan");
+stack.Push("Nazar");
+stack.Push("Olga");
+stack.Push("Kolya");
+stack.Push("Vika");
+
+Console.WriteLine(stack.Peek()); // відобразити елемент на черзі
+Console.WriteLine("Stack size: " + stack.Count);
+
+while (stack.Count > 0)
+    Console.WriteLine(stack.Pop());
+
+Console.WriteLine("Stack is empty!\n");
+
+// ----------- Queue -----------
+Queue<string> queue = new();
+
+queue.Enqueue("Ivan");
+queue.Enqueue("Nazar");
+queue.Enqueue("Olga");
+queue.Enqueue("Kolya");
+queue.Enqueue("Vika");
+
+Console.WriteLine(queue.Peek()); // відобразити елемент на черзі
+Console.WriteLine("Queue size: " + queue.Count);
+
+while (queue.Count > 0)
+    Console.WriteLine(queue.Dequeue());
+
+Console.WriteLine("Queue is empty!");
+
