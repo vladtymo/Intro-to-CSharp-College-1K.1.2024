c80d0d16a5ae5ec7916d803fc48f4a3de51aa03e
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2025-01-24 13:45:56 +0200

    Drum game

---
+++ 19_bandit/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    static Random rand = new();
+    private static int speed = 100; // чим менша затримка, тим швидше обертаються барабани
+
+    static Queue<char> drum1 = new(new[] {'7', '3', '♦', '♡', '$'});
+    static Queue<char> drum2 = new(new[] {'7', '3', '♦', '♡', '$'});
+    static Queue<char> drum3 = new(new[] {'7', '3', '♦', '♡', '$'});
+    
+    static void RotateDrum(Queue<char> drum)
+    {
+        for (int i = 0; i < rand.Next(5, 20); i++)
+        {
+            drum.Enqueue(drum.Dequeue());
+            Thread.Sleep(speed);
+            Console.Clear();
+            Console.WriteLine($"Result: [{drum1.Peek()}] [{drum2.Peek()}] [{drum3.Peek()}]");
+        }
+    }
+    
+    public static void Main(string[] args)
+    {
+        Console.WriteLine($"Result: [{drum1.Peek()}] [{drum2.Peek()}] [{drum3.Peek()}]");
+
+        while (true)
+        {
+            RotateDrum(drum1);
+            RotateDrum(drum2);
+            RotateDrum(drum3);
+
+            Console.WriteLine("Press any key to play again...");
+            Console.ReadKey();
+        }
+    }
+}
